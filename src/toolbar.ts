2362c07b3f34b9acb31a0dcaee718a357af751ca
Ilya Ovdin <iovdin@gmail.com>
2023-06-02 15:55:15 +0200

    show error, add output of cell to prompt

--- src/toolbar.ts
+++ src/toolbar.ts
@@ -1,6 +1,11 @@
 // @ts-nocheck
 import { IDisposable, DisposableDelegate } from '@lumino/disposable';
-import { NotebookPanel, INotebookModel } from '@jupyterlab/notebook';
+import {
+  NotebookPanel,
+  INotebookModel,
+  NotebookActions,
+} from '@jupyterlab/notebook';
+
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
 import { DocumentRegistry } from '@jupyterlab/docregistry';
 import { ToolbarButton } from '@jupyterlab/apputils';
@@ -43,15 +48,12 @@ export class CodexButtonExtension
         svgstr: CODEX_ICON,
       }),
       onClick: async () => {
+        window.widget = widget;
         const notebook = widget.content.widgets;
 
-        let passActive = false;
-        //let activeIndex = -1;
         const messages = notebook
+          .filter((cell: Cell, index) => index < widget.content.activeCellIndex)
           .map((cell: Cell, index) => {
-            if (passActive) {
-              return;
-            }
             const lines = cell.model.value.text.split('\n');
             let role = '';
             const header = lines[0].toLowerCase();
@@ -65,38 +67,63 @@ export class CodexButtonExtension
               return;
             }
 
-            if (cell === widget.content.activeCell) {
-              passActive = true;
-              //activeIndex = index;
+            let content = lines.slice(1).join('\n');
+            const outputContent = [];
+
+            const outputs = cell.model.outputs;
+            if (outputs) {
+              for (let i = 0; i < outputs.list.length; i++) {
+                const data = outputs.list.get(i).data;
+                for (const mimeType of [
+                  'application/vnd.jupyter.stdout',
+                  'application/vnd.jupyter.stderr',
+                  'text/plain',
+                ]) {
+                  if (data[mimeType]) {
+                    outputContent.push(data[mimeType]);
+                    break;
+                  }
+                }
+              }
             }
-            const content = lines.slice(1).join('\n');
+            if (outputContent.length) {
+              content += '\nOutput:\n' + outputContent.join('\n');
+            }
 
             return {
               role,
               content,
             };
-          })
-          .filter(item => item);
+          });
 
-        console.log(this.config);
         const { api_key, engine, max_tokens, temperature } = this.config;
         const xhr = new window.XMLHttpRequest();
         xhr.open('POST', 'https://api.openai.com/v1/chat/completions', true);
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.setRequestHeader('Authorization', 'Bearer ' + api_key);
         xhr.addEventListener('progress', () => {
-          const code = xhr.response
-            .toString()
-            .split('\n')
-            .map(item => item.trim())
-            .filter(item => !!item)
-            .map(item => item.replace(/^data: /, ''))
-            .filter(item => item !== '[DONE]')
-            .map(item => JSON.parse(item))
-            .map(item => item.choices[0].delta.content)
-            .join('');
+          // check error
+          let code = '';
+          try {
+            const res = JSON.parse(xhr.response.toString());
+            code = res.error.message;
+            console.log(res);
+          } catch (e) {
+            code = xhr.response
+              .toString()
+              .split('\n')
+              .map(item => item.trim())
+              .filter(item => !!item)
+              .map(item => item.replace(/^data: /, ''))
+              .filter(item => item !== '[DONE]')
+              .map(item => JSON.parse(item))
+              .map(item => item.choices[0].delta.content)
+              .join('');
+          }
 
           if (code && widget.content.activeCell) {
+            // NotebookActions.insertBelow(widget.content.activeCell);
+            // window.NotebookActions = NotebookActions
             widget.content.activeCell.model.value.text =
               '# assistant:\n' + code;
           }
